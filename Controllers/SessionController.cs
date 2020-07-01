7597839147b5d6aa67b6c8a0867dbe3a9a58fd20
binhdoitsme <binhdoitsme@gmail.com>
2020-07-01 21:57:31 +0700

    binhdh: added repository usage to SessionController

--- Controllers/SessionController.cs
+++ Controllers/SessionController.cs
@@ -6,6 +6,7 @@ using System.Security.Principal;
 using System.Threading.Tasks;
 using InstaminiWebService.Database;
 using InstaminiWebService.Models;
+using InstaminiWebService.Repositories;
 using InstaminiWebService.ResponseModels;
 using InstaminiWebService.ResponseModels.Factory;
 using InstaminiWebService.Utils;
@@ -22,27 +23,24 @@ namespace InstaminiWebService.Controllers
     public class SessionController : ControllerBase
     {
         private readonly InstaminiContext DbContext;
+        private readonly UserRepository Repository;
         private readonly IResponseModelFactory ResponseModelFactory;
 
-        public SessionController(InstaminiContext context, IResponseModelFactory responseModelFactory)
+        public SessionController(InstaminiContext context,
+                                IResponseModelFactory responseModelFactory,
+                                RepositoryFactory repositoryProvider)
         {
             DbContext = context;
             ResponseModelFactory = responseModelFactory;
+            Repository = (UserRepository)repositoryProvider.GetRepository<User>();
         }
 
-        [HttpPost] [AllowAnonymous]
+        [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> BeginSession([FromBody] User _user)
         {
             // validate username/password
-            User user = await DbContext.Users
-                                .Include(u => u.AvatarPhoto)
-                                .Include(u => u.Followers)
-                                    .ThenInclude(f => f.Follower)
-                                        .ThenInclude(f => f.AvatarPhoto)
-                                .Include(u => u.Followings)
-                                    .ThenInclude(f => f.User)
-                                        .ThenInclude(f => f.AvatarPhoto)
-                                .FirstOrDefaultAsync(u => u.Username == _user.Username);
+            User user = await Repository.FindByUsernameAsync(_user.Username);
             bool isValidUser = false;
             if (user != null)
             {
@@ -52,18 +50,19 @@ namespace InstaminiWebService.Controllers
             // if not valid username/password return BadRequest
             if (!isValidUser)
             {
-                return BadRequest(new { Err =  "Username or password is incorrect!" });
+                return BadRequest(new { Err = "Username or password is incorrect!" });
             }
 
             // else create and set-cookie JWT string
-            DbContext.Entry(user).CurrentValues.SetValues(new { LastLogin = DateTimeOffset.UtcNow });
-            await DbContext.SaveChangesAsync();
-            
+            await Repository.UpdateAsync(user, new User()
+            {
+                LastLogin = DateTimeOffset.UtcNow
+            });
+
             string jwt = JwtUtils.CreateJwt(user.Username, user.Id);
             var principal = JwtUtils.ValidateJWT(jwt);
             var userResponse = (UserResponse)ResponseModelFactory.Create(user);
 
-            HttpContext.Response.Cookies.Append("Token", jwt);
             return Ok(new
             {
                 Token = jwt,
@@ -76,7 +75,8 @@ namespace InstaminiWebService.Controllers
             });
         }
 
-        [HttpGet] [AllowAnonymous]
+        [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> ValidateSession([FromQuery(Name = "key")] string token)
         {
             var validatedPrincipal = JwtUtils.ValidateJWT(token);
@@ -86,29 +86,23 @@ namespace InstaminiWebService.Controllers
             }
 
             int userId = int.Parse(validatedPrincipal.FindFirstValue(ClaimTypes.NameIdentifier));
-            var validatedUser = await DbContext.Users.Include(u => u.AvatarPhoto)
-                                                .Include(u => u.Followers)
-                                                    .ThenInclude(f => f.Follower)
-                                                        .ThenInclude(f => f.AvatarPhoto)
-                                                .Include(u => u.Followings)
-                                                    .ThenInclude(f => f.User)
-                                                        .ThenInclude(f => f.AvatarPhoto)
-                                                .FirstOrDefaultAsync(u => u.Id == userId);
+            var validatedUser = await Repository.FindByIdAsync(userId);
             var userResponse = (UserResponse)ResponseModelFactory.Create(validatedUser);
 
-            return Ok(new 
+            return Ok(new
             {
                 Token = token,
-                userResponse.Id, 
-                userResponse.Username, 
-                userResponse.DisplayName, 
+                userResponse.Id,
+                userResponse.Username,
+                userResponse.DisplayName,
                 userResponse.AvatarLink,
                 userResponse.Followings,
-                userResponse.Link 
+                userResponse.Link
             });
         }
 
-        [HttpDelete] [Authorize]
+        [HttpDelete]
+        [Authorize]
         public IActionResult InvalidateSession([FromQuery(Name = "key")] string jwt)
         {
             var principal = JwtUtils.ValidateJWT(jwt);
@@ -116,7 +110,6 @@ namespace InstaminiWebService.Controllers
             {
                 return Unauthorized();
             }
-            Response.Cookies.Delete("Token"); 
             JwtUtils.InvalidateJWT(jwt);
             return Ok();
         }
